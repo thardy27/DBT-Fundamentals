2b9e5c645f1176c1c8c8f5b9c711d428fa75c1f2
Tobin Hardy <tobin.hardy@theinformationlab.co.uk>
2026-04-30 09:21:15 +0000

    stg_stripe__payments

--- models/Staging/stripe/stg_stripe__payments.sql
+++ models/Staging/stripe/stg_stripe__payments.sql
@@ -1,5 +1,28 @@
-    select 
-        *
+with 
 
+source as (
+
+    select *
     from 
-        raw.stripe.payment
+       raw.stripe.payment
+
+),
+
+renamed as (
+
+    select
+        id as payment_id
+        , orderid as order_id
+        , paymentmethod as payment_method
+        ,status as payment_method
+        ,amount as payment_amount
+        ,created as payment_created
+        ,_batched_at
+        
+    from source    
+
+)
+
+select *
+from
+    renamed
