3829586dce9c00eb28f48a7df99ce02d8743fca4
Tobin Hardy <tobin.hardy@theinformationlab.co.uk>
2026-04-30 12:49:25 +0000

    ds

--- models/Staging/stripe/stg_stripe__payments.sql
+++ models/Staging/stripe/stg_stripe__payments.sql
@@ -15,7 +15,8 @@ renamed as (
         , orderid as order_id
         , paymentmethod as payment_method
         ,status as payment_status
-        ,amount as payment_amount
+        -- amount is stored in cents. convert it to dollars
+        ,amount / 100 as payment_amount
         ,created as payment_created
         ,_batched_at
         
