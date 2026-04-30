686fee381e957aca997fd6a59688eb22c75a76ba
Tobin Hardy <tobin.hardy@theinformationlab.co.uk>
2026-04-30 11:06:37 +0000

    Replaced orders staging file

--- models/Staging/Jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/Staging/Jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,8 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        id,
+        user_id,
         order_date,
-        status
+        status,
+        _etl_loaded_at
 
-    from {{source('jaffle_shop', 'orders')}}
+    from source
+
+)
+
+select * from renamed
